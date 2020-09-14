eeecce53e810447e9ba9e727761501ec7680049f
Johan Planchon <johan.planchon@simplx.fr>
2020-09-14 10:00:21 +0200

    keymap

--- personal/keymap.c
+++ personal/keymap.c
@@ -81,6 +81,7 @@ enum layouts {
     SHIFTALT,
     CTRLALT,
     CTRLSHIFTALT,
+    MOUSEMOD
 };
 
 keymap_config_t keymap_config;
@@ -89,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEFAULT] = LAYOUT_65_ansi_blocker(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT, KC_PGUP, \
+        LT(MOUSEMOD,KC_CAPS),KC_A,KC_S,KC_D,KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT, KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,            KC_UP, KC_PGDN, \
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(ALT), MO(FUN), KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
@@ -99,6 +100,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
         _______, KC_LALT, KC_LGUI,                            _______,                            _______, _______, _______, _______, _______  \
+    ),
+    [MOUSEMOD] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_WH_U, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_R, _______, _______,          _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______  \
     ),
     [FUN] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
@@ -206,17 +214,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 led_instruction_t led_instructions[] = {
-    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = DEFAULT, .id0 = 4294967295, .id1 = 4294967295, .id2 = 7, .r = 255, .g = 255, .b = 255},//.r = 0, .g = 255, .b = 30 },
-    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = MACDEFA, .id0 = 4294967295, .id1 = 4294967295, .id2 = 7, .r = 255, .g = 255, .b = 255},//.r = 0, .g = 255, .b = 30 },
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = DEFAULT, .id0 = 4294967295, .id1 = 4294967295, .id2 = 7, .r = 255, .g = 255, .b = 255 },//.r = 0, .g = 255, .b = 30 },
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = DEFAULT, .id1 = 3221225472, .r = 255, .g = 255 },
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = MACDEFA, .id0 = 4294967295, .id1 = 4294967295, .id2 = 7, .r = 255, .g = 255, .b = 255 },//.r = 0, .g = 255, .b = 30 },
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = MACDEFA, .id1 = 3221225472, .r = 255, .g = 255 },
 
-    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = FUN, .id0 = 8190, .r = 255, .g = 0, .b = 0},
-    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = FUN, .id0 = 771768320, .id1 = 33687552, .r = 255, .g = 255, .b = 255},
-    //{ .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = 1, .id0 = 2149515264, .id1 = 8207, .r = 255, .g = 255, .b = 0},
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = MOUSEMOD, .id0 = 8388608, .id1 = 224, .r = 255, .g = 255 },
 
-    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = ALT, .id0 = 2152136704, .id1 = 537690114, .r = 255, .g = 255, .b = 255},
-    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = ALT, .id0 = 14, .r = 255, .g = 255, .b = 0 },
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = FUN, .id0 = 771768320, .id1 = 50464768, .id2 = 7, .r = 255 },
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = FUN, .id0 = 8190, .r = 255, .g = 255, .b = 255 },
+    //{ .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = 1, .id0 = 2149515264, .id1 = 8207, .r = 255, .g = 255, .b = 0 },
 
-    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = SHIFTALT, .id0 = 2152005632, .id1 = 536903682, .r = 255, .g = 255, .b = 255},
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = ALT, .id0 = 2168848384, .id1 = 537690114, .r = 255, .g = 255, .b = 255 },
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = ALT, .id0 = 14, .id1 = 67112960, .r = 255, .g = 255 },
+
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = SHIFTALT, .id0 = 2168717312, .id1 = 536903682, .r = 255, .g = 255, .b = 255 },
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = SHIFTALT, .id1 = 67108864, .r = 255, .g = 255 },
+
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = CTRLALT, .id0 = 2168782848, .r = 255, .g = 255, .b = 255 },
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = CTRLALT, .id1 = 4096, .r = 255, .g = 255 },
+    
+    { .flags = LED_FLAG_MATCH_LAYER | LED_FLAG_MATCH_ID | LED_FLAG_USE_RGB, .layer = CTRLSHIFTALT, .id0 = 2168782848, .r = 255, .g = 255, .b = 255 },
 
     { .end = 1 }
 };
